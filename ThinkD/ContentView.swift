a3f2f7e79ba95d944c4e4ac855daf2d118d84b48
Will Lyons <wlyons@gmail.com>
2025-03-04 15:44:03 -0800

    Initial Commit

---
+++ ThinkD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThinkD
+//
+//  Created by William Lyons on 2025-03-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThinkD/ThinkDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkDApp.swift
+//  ThinkD
+//
+//  Created by William Lyons on 2025-03-04.
+//
+
+import SwiftUI
+
+@main
+struct ThinkDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
